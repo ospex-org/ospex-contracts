f7d1f94222bbd772da11ba9ca8ffc695c8f6087c
Vince Laird <vincelaird@gmail.com>
2023-05-04 23:45:12 -0700

    initial commit with newest version of foundry

---
+++ src/CFPStructs.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+struct Contest {
+    uint32 awayScore; // final awayScore
+    uint32 homeScore; // final homeScore
+    address contestCreator;
+    uint8[2] scoringOracles; // oracle mappings to be used for scoring, must be defined during contest creation
+    ContestStatus contestStatus; // is contest final (and is score available to speculations)
+    string rundownId; // from rundown api
+    string sportspageId; // from sportspage api
+}
+
+enum ContestStatus {
+    Unverified,
+    Verified,
+    Pending,
+    Scored,
+    NotMatching,
+    ScoredManually,
+    RequiresConfirmation, // for 0-0 oracle-confirmed scores
+    Void // untested
+}
+
+// contest speculation definition: represents the speculative structure created off of contests
+struct Speculation {
+    uint256 contestId; // the contest id that this speculation relates to
+    uint256 upperAmount; // amount speculated on away side or the over
+    uint256 lowerAmount; // amount speculated on home side or the under
+    uint32 lockTime; // contest start time, contest is locked and no longer accepting speculations
+    address speculationScorer; // contest speculation type is determined from this address
+    int32 theNumber; // number that the speculation may be based on
+    address speculationCreator;
+    SpeculationStatus speculationStatus; // state of contract
+    WinSide winSide; // winning side of contest
+}
+
+enum SpeculationStatus {
+    Open,
+    Locked,
+    Closed
+}
+enum WinSide {
+    TBD,
+    Away,
+    Home,
+    Over,
+    Under,
+    Push, // push=tie
+    Forfeit, // forfeit=game canceled (or similar, manual process required to cancel)
+    Invalid, // invalid=all speculations on same side
+    Void // void=unresolved after variable void time and voided by user
+}
+enum PositionType {
+    Upper,
+    Lower
+}
+
+// user speculation represents any speculation on a contest made by a user
+struct Position {
+    uint256 upperAmount;
+    uint256 lowerAmount;
+    bool claimed;
+}
+
+struct Oracle {
+    address oracleAddress;
+    bytes32 jobId;
+    uint256 fee;
+}
---
+++ src/SpeculationSpread.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./CFPStructs.sol";
+
+interface IContestScorer {
+    function getContest(uint256 _id) external view returns (Contest memory);
+}
+
+contract SpeculationSpread is AccessControl {
+    error ZeroZeroScoreMustBeVerified();
+    error NonMatchingScoreFromOracles();
+    error ScoreNotFinalized(uint256 contestId);
+
+    // create role for contest contract address as this will be the only address calling this contract
+    bytes32 public constant CONTEST_CONTRACT_ADDRESS =
+        keccak256("CONTEST_CONTRACT_ADDRESS");
+    // create role for manager of contest address
+    bytes32 public constant SCOREMANAGER_ROLE = keccak256("SCOREMANAGER_ROLE");
+
+    address public ContestScorer;
+
+    constructor(address _contestScorer) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        ContestScorer = _contestScorer;
+    }
+
+    modifier nonZeroScoreContestResult(
+        uint32 _awayScore,
+        uint32 _homeScore,
+        ContestStatus _contestStatus
+    ) {
+        if (_contestStatus == ContestStatus.NotMatching) {
+            revert NonMatchingScoreFromOracles();
+        } else if (_awayScore + _homeScore == 0) {
+            revert ZeroZeroScoreMustBeVerified();
+        }
+        _;
+    }
+
+    function determineWinSide(
+        uint256 _id,
+        int32 _theNumber
+    )
+        external
+        view
+        onlyRole(CONTEST_CONTRACT_ADDRESS)
+        nonZeroScoreContestResult(
+            IContestScorer(ContestScorer).getContest(_id).awayScore,
+            IContestScorer(ContestScorer).getContest(_id).homeScore,
+            IContestScorer(ContestScorer).getContest(_id).contestStatus
+        )
+        returns (WinSide)
+    {
+        Contest memory contest = IContestScorer(ContestScorer).getContest(_id);
+        if (
+            !(contest.contestStatus == ContestStatus.Scored ||
+                contest.contestStatus == ContestStatus.ScoredManually)
+        ) {
+            revert ScoreNotFinalized(_id);
+        }
+        return scoreSpread(contest.awayScore, contest.homeScore, _theNumber);
+    }
+
+    function scoreSpread(
+        uint32 _awayScore,
+        uint32 _homeScore,
+        int32 _theNumber
+    ) private pure returns (WinSide) {
+        if (int32(_awayScore) + _theNumber >= int32(_homeScore)) {
+            return WinSide.Away;
+        } else {
+            return WinSide.Home;
+        }
+    }
+
+    // set the interface address for the current contest logic
+    function setContractInterfaceAddress(
+        address _address
+    ) external onlyRole(SCOREMANAGER_ROLE) {
+        ContestScorer = _address;
+    }
+}
